86719af08619560292b5d7f8306451cfda4084ab
Le Tuan Canh <ltcanh.itdevelop@mail.com>
2023-04-06 15:10:04 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<!-- @format -->
+
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
